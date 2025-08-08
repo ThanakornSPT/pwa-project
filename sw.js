e410dad18c295e5f016298503bea14e822d889d4
Nick <thanakornsukpitak@gmail.com>
2025-08-08 16:35:51 +0700

    edit homepage default

--- sw.js
+++ sw.js
@@ -22,11 +22,11 @@ const STATIC_ASSETS = [
 
 // Assets to cache on first use (dynamic)
 const DYNAMIC_ASSETS = [
-    './assets/icons/icon-72x72.png',
-    './assets/icons/icon-96x96.png',
-    './assets/icons/icon-128x128.png',
-    './assets/icons/icon-192x192.png',
-    './assets/icons/icon-512x512.png'
+    './assets/icons/icon-72x72.jpg',
+    './assets/icons/icon-96x96.jpg',
+    './assets/icons/icon-128x128.jpg',
+    './assets/icons/icon-192x192.jpg',
+    './assets/icons/icon-512x512.jpg'
 ];
 
 // Install event - Cache static assets
